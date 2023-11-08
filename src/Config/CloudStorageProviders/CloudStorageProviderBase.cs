7042f191f1302582767073d3635687af48c8c9dd
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-11-08 10:31:53 -0800

    Implement Cloud storage

---
+++ src/Config/CloudStorageProviders/CloudStorageProviderBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace BirthdayNotifications.Config.CloudStorageProviders {
+  /// <summary>
+  /// TODO: Descriptor
+  /// </summary>
+  [Serializable]
+  public class CloudStorageProviderBase {
+    /// <summary>
+    /// TODO: Descriptor
+    /// </summary>
+    [JsonProperty("name", Order = 1, Required = Required.Always)]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// TODO: Descriptor
+    /// </summary>
+    [JsonProperty("api_key", Order = 2, Required = Required.AllowNull)]
+    public string? ApiKey { get; set; } = string.Empty;
+  }
+}
